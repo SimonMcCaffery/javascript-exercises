7622193d441621da3199ae03647cf9bc2a5f9ce2
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-14 22:28:14 +0000

    npm issues
    seemingly fixed memory issues and completed reverse string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,11 @@
-const repeatString = function(string, repeat) {
-    let i = repeat;
-    let result =""
-
-    while (i>=0){
-        result+=string;
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
     }
-    return result;
-};
+    return string;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let text = str;
+    const textArray = text.split("");
+    let reverseArray = textArray.reverse();
+    let reverseJoined = reverseArray.join("");
+    return reverseJoined
 };
 
 // Do not edit below this line
