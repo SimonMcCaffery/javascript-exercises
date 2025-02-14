093883239f10dafddb57f9d73663830d19e2a12b
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-14 21:17:49 +0000

    1-2
    Exercise 1 and 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    let i = repeat;
+    let result =""
 
+    while (i>=0){
+        result+=string;
+    }
+    return result;
 };
 
 // Do not edit below this line
