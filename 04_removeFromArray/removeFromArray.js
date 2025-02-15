41d1cb9a55877533edb00ca991e4da7c57a62359
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-15 13:22:29 +0000

    completed 04 and 05
    Completed exercises 4 and 5
    
    still making heavy use of chart gpt to find out what functions I should be using for each step of the problem and understanding gaps but making improvement I think

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+
+    const removeSet = new Set(valuesToRemove)
+    return arr.filter(item => !removeSet.has(item))
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    const range = [];
 
-};
+    if(Number.isInteger(start) && Number.isInteger(end) && start>= 0 && end>= 0){        
+        if (start < end) {
+            for (i = start; i <= end; i++) {
+                range.push(i);
+            }
+        } else {
+            for (i = end; i <= start; i++) {
+                range.push(i);
+            }
+        }
+        const sum = range.reduce((acc, num) => acc + num, 0)
+        return sum;
+    }else{
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
