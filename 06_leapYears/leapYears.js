e5d390af8748a73538ada1133ed26548d213be6a
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-15 15:04:28 +0000

    completed 06 07
    Completed exercise 06 and 07, easier than array exercises earlier or at least I could come up with how to solve the problem on my own

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (val) {
+    if (val % 4 == 0) {
+        if (val % 100 == 0 && val % 400 !== 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  let toCel = (val -32) * 5/9;
+  if(Number.isInteger(toCel)){
+    return toCel
+  }else{
+    let roundCel = Math.round(toCel*10)/10;
+    return roundCel;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+  let toFahr = (val * 9/5)+32;  
+  if(Number.isInteger(toFahr)){
+    return toFahr
+  }else{
+    let roundFahr = Math.round(toFahr * 10)/10;
+    return roundFahr;
+  }
 };
 
 // Do not edit below this line
